beb6f8d04e20f74ff7fb73df77d996f9b67a2de3
Prashant bhatt <93563996+Prashant2242@users.noreply.github.com>
2025-01-29 13:04:56 +0530

    Add files via upload

---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,10 @@
+const heading = React.createElement("h1", {
+    id: "heading"
+}, "Namaste eveeryone!");
+const parent = React.createElement("div", {
+    id: "parent"
+}, React.createElement("div", {}, "I am an h1 tag"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
